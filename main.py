9f38167a50ede3749c3f1877c57f02f6fa1fdba9
Mikola3 <34712864+Mikola3@users.noreply.github.com>
2018-07-17 11:02:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
-
+app.debug = True
+    
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
-
+    return 'Hello, World!'
+    
+@app.route('/hello')
+def hello_path():
+    return "HELLO FROM PATH"
+    
 if __name__ == '__main__':
-  app.run()
+    app.run()
